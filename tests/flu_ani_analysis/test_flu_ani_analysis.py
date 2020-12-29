44438f5b34eb3f67fd92cedcfdd1efaf2c3a6d47
Mariusz Las <72602645+mariuszlas@users.noreply.github.com>
2020-12-29 12:50:14 +0100

    Update test_flu_ani_analysis.py
    Added a new test for the visualise function.
    Updated all tests by adding the platemap_csv parameter to read_in_envision function.

--- tests/flu_ani_analysis/test_flu_ani_analysis.py
+++ tests/flu_ani_analysis/test_flu_ani_analysis.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import pytest
 import pickle
-from flu_ani_analysis.flu_ani_analysis_module import FA as fa
-
-# define well plate dimensions
-plate_dimensions = {'96':(8, 12), '384':(16, 24)}
-    
+import os
+import matplotlib.pyplot as plt
+from matplotlib.testing.compare import compare_images
+from flu_ani_analysis.flu_ani_analysis_module import FA as fa    
 
 # raw data files in .csv format
 plate_1 = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\plate1.csv"
@@ -33,6 +32,16 @@ list_B_repeat_end_test = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-
 list_B_repeat_96_test = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\example outputs\\list_B_repeat_96_test.pkl"
 list_C_test = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\example outputs\\list_C_test.pkl"
 
+
+HsHis6_PEX5C_vs_HsPEX5C = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\Hs-His6-PEX5C vs HsPEX5C.csv"
+HsPEX5C_Y467C_vs_AtPEX5C_WT = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\HsPEX5C Y467C vs AtPEX5C WT.csv"
+F606C_vs_AtPEX5C_WT_1_hour = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\F606C vs AtPEX5C WT 1 hour.csv"
+
+plate_map_file = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\plate_map.csv"
+Hs_His6_PEX5C_vs_HsPEX5C_platemap = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\Hs-His6-PEX5C vs HsPEX5C platemap.csv"
+HsPEX6C_Y467C_vs_AtPEX5C_WT_platemap = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\HsPEX6C Y467C vs AtPEX5C WT platemap.csv"
+F606C_vs_AtPEX5C_WT_1_hour_platemap = "C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\Test data\\F606C vs AtPEX5C WT 1 hour platemap.csv"
+
 # expected values of g-factor
 expected_g = {'plate_1': 1.15, 'plate_1_repeat': 1.15, 'plate_2_1': 1.15, 'plate_2_repeat': 1.15, 'plate_2_repeat_96': 1.0, 
         'list_A': 1.15, 'list_A_repeat': 1.15, 'list_B': 1.15, 'list_B_repeat_96': 1.0, 'list_B_repeat_end': 1.15, 'list_C': 1.15}
@@ -42,12 +51,12 @@ expected_g = {'plate_1': 1.15, 'plate_1_repeat': 1.15, 'plate_2_1': 1.15, 'plate
 def get_testing_data():
     """Set up for the first 11 tests."""
     
-    def _get_testing_data(csv_file, data_type, size, pkl_file):
+    def _get_testing_data(data_csv, platemap_csv, data_type, size, pkl_file):
         
         with open(pkl_file, 'rb') as file:   # load the list with expexcted data frames from .pkl file
             expected_list = pickle.load(file)
 
-        actual_output = fa.read_in_envision(csv_file = csv_file, data_type=data_type, size=size)   # execute the tested function
+        actual_output = fa.read_in_envision(data_csv = data_csv, platemap_csv=platemap_csv, data_type=data_type, size=size)   # execute the tested function
         actual_g = actual_output.g_factor
         actual_list = []
 
@@ -65,15 +74,15 @@ def get_testing_data():
 
 def test_plate_1(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=plate_1, data_type='plate', size=384, pkl_file=plate_1_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=plate_1, platemap_csv=plate_map_file, data_type='plate', size=384, pkl_file=plate_1_test)
     assert g_factor == expected_g['plate_1']   # test for the g-factor
-    for actual_df, expected_df in zip(actual_list, expected_list):   # zip the two lists containg actual and expected data frames into a list of tuples and iterate over all tuples
+    for actual_df, expected_df in zip(actual_list, expected_list):   # zip the two lists containing actual and expected data frames into a list of tuples and iterate over all tuples
         pd.testing.assert_frame_equal(actual_df, expected_df)   # test whether actual and expected dfs are the same
         
         
 def test_plate_1_repeat(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=plate_1_repeat, data_type='plate', size=384, pkl_file=plate_1_repeat_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=plate_1_repeat, platemap_csv=plate_map_file, data_type='plate', size=384, pkl_file=plate_1_repeat_test)
     assert g_factor == expected_g['plate_1_repeat']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -81,7 +90,7 @@ def test_plate_1_repeat(get_testing_data):
         
 def test_plate_2_1(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=plate_2_1, data_type='plate', size=384, pkl_file=plate_2_1_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=plate_2_1, platemap_csv=plate_map_file, data_type='plate', size=384, pkl_file=plate_2_1_test)
     assert g_factor == expected_g['plate_2_1']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -89,7 +98,7 @@ def test_plate_2_1(get_testing_data):
         
 def test_plate_2_repeat(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=plate_2_repeat, data_type='plate', size=384, pkl_file=plate_2_repeat_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=plate_2_repeat, platemap_csv=plate_map_file, data_type='plate', size=384, pkl_file=plate_2_repeat_test)
     assert g_factor == expected_g['plate_2_repeat']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -97,7 +106,7 @@ def test_plate_2_repeat(get_testing_data):
         
 def test_plate_2_repeat_96(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=plate_2_repeat_96, data_type='plate', size=96, pkl_file=plate_2_repeat_96_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=plate_2_repeat_96, platemap_csv=plate_map_file, data_type='plate', size=96, pkl_file=plate_2_repeat_96_test)
     assert g_factor == expected_g['plate_2_repeat_96']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -105,7 +114,7 @@ def test_plate_2_repeat_96(get_testing_data):
         
 def test_list_A(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=list_A, data_type='list', size=384, pkl_file=list_A_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=list_A, platemap_csv=plate_map_file, data_type='list', size=384, pkl_file=list_A_test)
     assert g_factor == expected_g['list_A']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -113,7 +122,7 @@ def test_list_A(get_testing_data):
         
 def test_list_A_repeat(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=list_A_repeat, data_type='list', size=384, pkl_file=list_A_repeat_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=list_A_repeat, platemap_csv=plate_map_file, data_type='list', size=384, pkl_file=list_A_repeat_test)
     assert g_factor == expected_g['list_A_repeat']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -121,7 +130,7 @@ def test_list_A_repeat(get_testing_data):
         
 def test_list_B(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=list_B, data_type='list', size=384, pkl_file=list_B_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=list_B, platemap_csv=plate_map_file, data_type='list', size=384, pkl_file=list_B_test)
     assert g_factor == expected_g['list_B']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -129,7 +138,7 @@ def test_list_B(get_testing_data):
         
 def test_list_B_repeat_end(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=list_B_repeat_end, data_type='list', size=384, pkl_file=list_B_repeat_end_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=list_B_repeat_end, platemap_csv=plate_map_file, data_type='list', size=384, pkl_file=list_B_repeat_end_test)
     assert g_factor == expected_g['list_B_repeat_end']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -137,7 +146,7 @@ def test_list_B_repeat_end(get_testing_data):
         
 def test_list_B_repeat_96(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=list_B_repeat_96, data_type='list', size=96, pkl_file=list_B_repeat_96_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=list_B_repeat_96, platemap_csv=plate_map_file, data_type='list', size=96, pkl_file=list_B_repeat_96_test)
     assert g_factor == expected_g['list_B_repeat_96']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -145,7 +154,7 @@ def test_list_B_repeat_96(get_testing_data):
         
 def test_list_C(get_testing_data):
     
-    actual_list, expected_list, g_factor = get_testing_data(csv_file=list_C, data_type='list', size=384, pkl_file=list_C_test)
+    actual_list, expected_list, g_factor = get_testing_data(data_csv=list_C, platemap_csv=plate_map_file, data_type='list', size=384, pkl_file=list_C_test)
     assert g_factor == expected_g['list_C']
     for actual_df, expected_df in zip(actual_list, expected_list):
         pd.testing.assert_frame_equal(actual_df, expected_df)
@@ -155,18 +164,41 @@ def test_list_C(get_testing_data):
 def test_plate_size_error():
     """Test for error raised if size is not 384 or 96."""
     
-    test_object = fa.read_in_envision(csv_file=plate_1, data_type='plate', size=100)
+    test_object = fa.read_in_envision(data_csv=plate_1, platemap_csv=plate_map_file, data_type='plate', size=100)
 
 
 @pytest.mark.raises()
 def test_incorrect_data_type_list():
     """Test for error if data_type = list but raw data file is in plate format."""
     
-    test_object = fa.read_in_envision(csv_file=plate_1, data_type='list', size=384)
+    test_object = fa.read_in_envision(data_csv=plate_1, platemap_csv=plate_map_file, data_type='list', size=384)
     
     
 @pytest.mark.raises()
 def test_incorrect_data_type_plate():
-     """Test for error if data_type = plate but raw data file is in list format."""
+    """Test for error if data_type = plate but raw data file is in list format."""
         
-    test_object = fa.read_in_envision(csv_file=list_A, data_type='plate', size=384)
+    test_object = fa.read_in_envision(data_csv=list_A, platemap_csv=plate_map_file, data_type='plate', size=384)
+    
+    
+@pytest.mark.raises()
+def test_incorrect_data_type():
+    """Test for error if data_type argument is neither plate nor list."""
+        
+    test_object = fa.read_in_envision(data_csv=list_A, platemap_csv=plate_map_file, data_type='typo', size=384)
+    
+    
+def test_visualise():
+    
+    actual_img = 'C:\\Users\\Bartek\\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\result_images\\actual_img.png'
+    expected_img = 'C:\\Users\\Bartek\Documents\\Fluorescence-Anisotropy-Analysis\\tests\\baseline_images\\expected_img.png'
+    
+    test_object = fa.read_in_envision(data_csv=HsHis6_PEX5C_vs_HsPEX5C, platemap_csv=Hs_His6_PEX5C_vs_HsPEX5C_platemap, data_type='plate', size=384)
+    test_object.visualise(labelby='Protein Concentration', colorby='Type', title='Test plot', export=actual_img)
+    
+    result = compare_images(expected_img, actual_img, tol=1)
+    
+    if os.path.isfile(actual_img):
+        os.remove(actual_img)
+    
+    assert result == None
