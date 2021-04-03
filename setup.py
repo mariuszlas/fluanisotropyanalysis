1d13984341e2923f9f057655540efec6b2dbed91
Mariusz Las <72602645+mariuszlas@users.noreply.github.com>
2021-04-03 15:19:24 +0100

    Update for v1.0.0

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(name='flu_ani_analysis',
-        version='0.1.0',
-        author='Mariusz Las',
-        author_email='cm18mel@leeds.ac.uk',
-        url = 'https://github.com/mariuszlas',
-        description='Fluorescence anisotropy binding data analysis.',
-        packages=['flu_ani_analysis'],
-        install_requires=['matplotlib', 'numpy', 'pandas'])
+with open("README.md", "r") as rd:
+    long_description = rd.read()
+
+setuptools.setup(
+        name='fluanisotropyanalysis',
+        version='1.0.0',
+        author='Mariusz Las, Stuart Warriner',
+        author_email='mariusz.las@outlook.com, s.l.warriner@leeds.ac.uk',
+        url = 'https://github.com/mariuszlas/Fluorescence-Anisotropy-Analysis',
+        description='Read and analyse data from fluorescence anisotropy assays.',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.8',
+        setup_requires=['wheel'],
+        install_requires=['platemapping']
+)
